a874068224377f12ce9d7bf00780f716bc13ec03
Laurent Bovet <laurent.bovet@windmaster.ch>
2023-04-08 22:10:07 +0200

    chore: simplify var rendering

--- src/impl/view/button.php
+++ src/impl/view/button.php
@@ -1,6 +1,6 @@
-<div class="litbee-button" onclick="window.location.replace('<?php print $accessGateUrl ?>');">
+<div class="litbee-button" onclick="window.location.replace('<?= $accessGateUrl ?>');">
     <div class="litbee-price">
-        <?php print $priceTag ?>
+        <?= $priceTag ?>
     </div>
     <div class="litbee-icon">
         <?php include('icon.php') ?>
